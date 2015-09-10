d88d8a3110cb03dc446ec90be6763c61b25737ee
Vibin <lwb_vibin@foxmail.com>
2015-09-10 22:55:42 +0800

    Create the original files

---
+++ Verkits.js
@@ -0,0 +1,8 @@
+/*
+ * Name: Verkits                                      
+ * Description: The kits for verification using JQuery.
+ * Create: 2015/09/09
+ * Modify: 2015/09/10                                  
+ * Author: Vibin
+ * Version: 1.0
+ */
